9970c1bedcbbf503fd2e6d6326b73bc89c9586ea
Adrian Latorre <94648244+Shinobirun@users.noreply.github.com>
2023-04-14 13:47:01 -0300

    AutomatizoIngreso

--- src/Latorre/ejercicio1/Main.java
+++ src/Latorre/ejercicio1/Main.java
@@ -1,6 +1,11 @@
 package Latorre.ejercicio1;
 
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static Latorre.ejercicio1.BuscLetraString.ordenarNumeros;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -12,16 +17,17 @@ public class Main {
         System.out.println("La letra '" + letra + "' aparece " + cantidad + " veces en la palabra '" + palabra + "'");
 
         // Ejemplo de uso del método ordenarNumeros
-        int a = 10;
-        int b = 5;
-        int c = 20;
-        boolean ascendente = false;
-        int[] numerosOrdenados = BuscLetraString.ordenarNumeros(a, b, c, ascendente);
-        System.out.println("Los números ordenados de manera " + (ascendente ? "ascendente" : "descendente") + " son: ");
-        for (int numero : numerosOrdenados) {
-            System.out.print(numero + " ");
-        }
-        System.out.println();
+
+        //int a = 10;
+        //int b = 5;
+        //int c = 20;
+        //boolean ascendente = false;
+        //int[] numerosOrdenados = BuscLetraString.ordenarNumeros(a, b, c, ascendente);
+        //System.out.println("Los números ordenados de manera " + (ascendente ? "ascendente" : "descendente") + " son: ");
+        //for (int numero : numerosOrdenados) {
+        //    System.out.print(numero + " ");
+        //
+        //System.out.println();
 
         // Ejemplo de uso del método sumarNumerosMayoresQueX
         int[] numeros = { 1, 2, 3, 4, 5 };
@@ -40,6 +46,46 @@ public class Main {
         System.out.println("Mensaje original: " + mensaje);
         System.out.println("Mensaje cifrado: " + mensajeCifrado);
         System.out.println("Mensaje descifrado: " + mensajeDescifrado);
+
+
+        //
+        int a, b, c;
+        boolean ascendente;
+        char orden = 'n';
+
+        if (args.length == 4) {
+            // Si hay 4 argumentos, asumimos que son a, b, c y ascendente
+            a = Integer.parseInt(args[0]);
+            b = Integer.parseInt(args[1]);
+            c = Integer.parseInt(args[2]);
+            ascendente = args[3].equalsIgnoreCase("ascendente");
+        } else {
+            // Si no hay 4 argumentos, solicitamos los valores por consola
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Ingrese el valor de a: ");
+            a = scanner.nextInt();
+            System.out.print("Ingrese el valor de b: ");
+            b = scanner.nextInt();
+            System.out.print("Ingrese el valor de c: ");
+            c = scanner.nextInt();
+            System.out.println("Ingrese 'a' para ordenar de forma ascendente o 'd' para ordenar de forma descendente:");
+            while (true) {
+                orden = scanner.next().charAt(0);
+                if (orden == 'a' || orden == 'd') {
+
+                    break;
+                }
+                System.out.println("Por favor, ingrese 'a' o 'd'.");
+            }
+        }
+        ascendente = (orden == 'a');
+
+        int[] numerosOrdenados = BuscLetraString.ordenarNumeros(a, b, c, ascendente);
+        System.out.println("Los números ordenados son: " + Arrays.toString(numerosOrdenados));
+
+
+
+
     }
 
 
