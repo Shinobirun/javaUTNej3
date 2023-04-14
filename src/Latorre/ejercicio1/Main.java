f7c081d76bc073f4d05dc10b7b6bff8c935aa195
Adrian Latorre <94648244+Shinobirun@users.noreply.github.com>
2023-04-14 13:48:03 -0300

    saco la llamada anterior

--- src/Latorre/ejercicio1/Main.java
+++ src/Latorre/ejercicio1/Main.java
@@ -16,18 +16,7 @@ public class Main {
         int cantidad = BuscLetraString.contarLetra(palabra, letra);
         System.out.println("La letra '" + letra + "' aparece " + cantidad + " veces en la palabra '" + palabra + "'");
 
-        // Ejemplo de uso del método ordenarNumeros
-
-        //int a = 10;
-        //int b = 5;
-        //int c = 20;
-        //boolean ascendente = false;
-        //int[] numerosOrdenados = BuscLetraString.ordenarNumeros(a, b, c, ascendente);
-        //System.out.println("Los números ordenados de manera " + (ascendente ? "ascendente" : "descendente") + " son: ");
-        //for (int numero : numerosOrdenados) {
-        //    System.out.print(numero + " ");
-        //
-        //System.out.println();
+       
 
         // Ejemplo de uso del método sumarNumerosMayoresQueX
         int[] numeros = { 1, 2, 3, 4, 5 };
